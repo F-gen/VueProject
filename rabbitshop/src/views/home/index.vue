79e24a56630324b411f10d3a2697fd17e863290c
F-gen <1101853378@qq.com>
2022-05-30 23:28:14 +0800

    feat scroll

--- rabbitshop/src/views/home/index.vue
+++ rabbitshop/src/views/home/index.vue
@@ -4,6 +4,8 @@
       <div class="container">
         <!-- 左侧分类 -->
         <HomeCategory />
+         <!-- 轮播图 -->
+         <HomeBanner />
       </div>
 
     </div>
@@ -11,9 +13,10 @@
 </template>
 <script>
 import HomeCategory from './components/home-category'
+import HomeBanner from './components/home-banner'
 export default {
   name: 'PageHome',
-  components: { HomeCategory }
+  components: { HomeCategory, HomeBanner }
 }
 </script>
 <style scoped lang="less"></style>

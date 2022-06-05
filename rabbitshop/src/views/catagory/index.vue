b17cf4a9c2694baa5def4a4ddc3f8533e1dd559f
F-gen <1101853378@qq.com>
2022-06-05 22:44:34 +0800

    feat bread

--- rabbitshop/src/views/catagory/index.vue
+++ rabbitshop/src/views/catagory/index.vue
@@ -1,17 +1,131 @@
 <template>
-<div class="TopCategory">
-
-</div>
+  <div class="top-category">
+    <div class="container">
+      <!-- 面包屑 -->
+      <XtxBread>
+        <XtxBreadItem to="/">首页</XtxBreadItem>
+        <XtxBreadItem >{{TopCategory.name}}</XtxBreadItem>
+      </XtxBread>
+      <!-- 轮播图 -->
+      <XtxCarousel :sliders="sliders" style="height:500px" />
+      <!-- 所有二级分类 -->
+      <div class="sub-list" >
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="sub in TopCategory.children" :key="sub.id">
+            <a href="javascript:;">
+              <img :src="sub.picture">
+              <p>{{sub.name}}</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <!-- 不同分类商品 -->
+      <div class="ref-goods">
+        <div class="head">
+          <h3>- 海鲜 -</h3>
+          <p class="tag">温暖柔软，品质之选</p>
+          <XtxMore />
+        </div>
+        <div class="body">
+          <GoodsItem v-for="i in 5" :key="i" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-
 <script>
+import { computed, ref } from 'vue'
+import { findBanner } from '@/api/home'
+import { useStore } from 'vuex'
+import { useRoute } from 'vue-router'
+import GoodsItem from './components/goods-item'
 export default {
   name: 'TopCategory',
+  components: { GoodsItem },
   setup () {
-    return {}
+    // 轮播图
+    const sliders = ref([])
+    findBanner().then(data => {
+      sliders.value = data.result
+    })
+    // 面包屑 +子分类
+    const store = useStore()
+    const route = useRoute()
+    const TopCategory = computed(() => {
+      // 当前顶级分类 等于 根据路由id 去veux 中category 模块的list 查找
+      let cate = {}
+      const item = store.state.category.list.find(item => {
+        return item.id === route.params.id
+      })
+      if (item) cate = item
+      return cate
+    })
+    return { sliders, TopCategory }
   }
 }
 </script>
-<style lang="less" scoped>
-
+<style scoped lang="less">
+.top-category {
+  h3 {
+    font-size: 28px;
+    color: #666;
+    font-weight: normal;
+    text-align: center;
+    line-height: 100px;
+  }
+  .sub-list {
+    margin-top: 20px;
+    background-color: #fff;
+    ul {
+      display: flex;
+      padding: 0 32px;
+      flex-wrap: wrap;
+      li {
+        width: 168px;
+        height: 160px;
+        a {
+          text-align: center;
+          display: block;
+          font-size: 16px;
+          img {
+            width: 100px;
+            height: 100px;
+          }
+          p {
+            line-height: 40px;
+          }
+          &:hover {
+            color: @xtxColor;
+          }
+        }
+      }
+    }
+  }
+ .ref-goods {
+    background-color: #fff;
+    margin-top: 20px;
+    position: relative;
+    .head {
+      .xtx-more {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+      }
+      .tag {
+        text-align: center;
+        color: #999;
+        font-size: 20px;
+        position: relative;
+        top: -20px;
+      }
+    }
+    .body {
+      display: flex;
+      justify-content: flex-start;
+      flex-wrap: wrap;
+      padding: 0 65px 30px;
+    }
+  }
+}
 </style>

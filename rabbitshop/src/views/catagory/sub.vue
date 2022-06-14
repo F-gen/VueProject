6cfad9514d1aa00a42beb1eef7dd45eeaa3b4b76
F-gen <1101853378@qq.com>
2022-06-14 23:25:26 +0800

    inifinite_loading

--- rabbitshop/src/views/catagory/sub.vue
+++ rabbitshop/src/views/catagory/sub.vue
@@ -16,27 +16,69 @@
             <GoodsItem :goods="{}" />
           </li>
         </ul>
+        <XtxInfiniteLoading :loading="loading" :finished="finished" @infinite="getData" />
       </div>
   </div>
 </div>
 </template>
 
 <script>
-import { ref } from 'vue'
-import SubBread from './components/sub-bread.vue'
+import SubBread from './components/sub-bread'
 import SubFilter from './components/sub-filter'
 import SubSort from './components/sub-sort'
 import GoodsItem from './components/goods-item'
+import { ref, watch } from 'vue'
+import { findSubCategoryGoods } from '@/api/category'
+import { useRoute } from 'vue-router'
 export default {
   name: 'SubCategory',
-  setup() {
-    const isChecked = ref(true)
-    return {
-      isChecked
-    }
-  },
-  components: { SubBread, SubFilter, SubSort, GoodsItem }
+  components: { SubBread, SubFilter, SubSort, GoodsItem },
+  setup () {
+    // 1. 基础布局
+    // 2. 无限加载组件
+    // 3. 动态加载数据且渲染
+    // 4. 任何筛选条件变化需要更新列表
+    const route = useRoute()
 
+    const loading = ref(false)
+    const finished = ref(false)
+    const goodsList = ref([])
+    // 查询参数
+    let reqParams = {
+      page: 1,
+      pageSize: 20
+    }
+    // 获取数据函数
+    const getData = () => {
+      loading.value = true
+      reqParams.categoryId = route.params.id
+      findSubCategoryGoods(reqParams).then(({ result }) => {
+        if (result.items.length) {
+          goodsList.value.push(...result.items)
+          reqParams.page++
+        } else {
+          // 加载完毕
+          finished.value = true
+        }
+        // 请求结束
+        loading.value = false
+      })
+    }
+
+    // 切换二级分类重新加载
+    watch(() => route.params.id, (newVal) => {
+      if (newVal && route.path === ('/category/sub/' + newVal)) {
+        goodsList.value = []
+        reqParams = {
+          page: 1,
+          pageSize: 20
+        }
+        finished.value = false
+      }
+    })
+
+    return { loading, finished, goodsList, getData }
+  }
 }
 </script>
 <style lang="less" scoped>

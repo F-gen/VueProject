ae6fa54c69999b94999ff1228a780a9bbef2af1d
F-gen <1101853378@qq.com>
2022-06-22 00:03:52 +0800

    goods

--- rabbitshop/src/views/goods/index.vue
+++ rabbitshop/src/views/goods/index.vue
@@ -9,7 +9,15 @@
         <XtxBreadItem>{{goods.name}}</XtxBreadItem>
       </XtxBread>
       <!-- 商品信息 -->
-      <div class="goods-info"></div>
+      <div class="goods-info">
+         <div class="media">
+           <GoodsImage :images="goods.mainPictures" />
+            <GoodsSales />
+         </div>
+        <div class="spec">
+              <GoodsName :goods="goods"/>
+        </div>
+      </div>
       <!-- 商品推荐 -->
       <GoodsRelevant />
       <!-- 商品详情 -->
@@ -29,12 +37,15 @@
 
 <script>
 import GoodsRelevant from './components/goods-relevant'
+import GoodsImage from './components/goods-image'
+import GoodsSales from './components/goods-sales'
+import GoodsName from './components/goods-name'
 import { nextTick, ref, watch } from 'vue'
 import { findGoods } from '@/api/product'
 import { useRoute } from 'vue-router'
 export default {
   name: 'XtxGoodsPage',
-  components: { GoodsRelevant },
+  components: { GoodsRelevant, GoodsImage, GoodsSales, GoodsName },
   setup () {
     const goods = useGoods()
 
@@ -66,6 +77,16 @@ const useGoods = () => {
 .goods-info {
   min-height: 600px;
   background: #fff;
+  display: flex;
+  .media {
+    width: 580px;
+    height: 600px;
+    padding: 30px 50px;
+  }
+  .spec {
+    flex: 1;
+    padding: 30px 30px 30px 0;
+  }
 }
 .goods-footer {
   display: flex;
